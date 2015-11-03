c130030a840f534e1590d1ee4e8a92ae1560de71
Mads Kalør <mads@kaloer.com>
2015-11-03 08:15:24 +0100

    Use Quack for Scheme

--- languages/scheme.el
+++ languages/scheme.el
@@ -1,2 +1,6 @@
 ;; Use petite-chez-scheme
-(setq scheme-program-name "petite")
+;(setq scheme-program-name "petite") ; Does not work at the moment
+; Use gauche scheme
+(setq scheme-program-name "gosh")
+(require 'quack)
+(setq quack-fontify-style 'emacs)
