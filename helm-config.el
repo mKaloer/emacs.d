82c87b8851562495ec99a9ae0b0468e136c1ed69
Mads Ellersgaard Kalør <mek@tradeshift.com>
2015-11-10 19:53:17 +0100

    Hide boring files in Helm

--- helm-config.el
+++ helm-config.el
@@ -12,7 +12,8 @@
       helm-move-to-line-cycle-in-source     t ; move to end or beginning of source when reaching top or bottom of source.
       helm-ff-search-library-in-sexp        t ; search for library in `require' and `declare-function' sexp.
       helm-scroll-amount                    8 ; scroll 8 lines other window using M-<next>/M-<prior>
-      helm-ff-file-name-history-use-recentf t)
+      helm-ff-file-name-history-use-recentf t ;
+      helm-ff-skip-boring-files             t)
 
 ;; The default "C-x c" is quite close to "C-x C-c", which quits Emacs.
 ;; Changed to "C-c h". Note: We must set "C-c h" globally, because we
