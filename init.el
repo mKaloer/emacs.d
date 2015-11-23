b175ddd070ff2ce9248b7a549f66d2898455eb76
Mads Kalør <mads@kaloer.com>
2015-11-23 22:11:53 +0100

    Start migration to use-package

--- init.el
+++ init.el
@@ -11,21 +11,26 @@
   (exec-path-from-shell-initialize))
 
 ; Recent files
-(require 'recentf)
-(setq recentf-auto-cleanup 'never) ;; disable before we start recentf!
-(recentf-mode 1)
-(setq recentf-max-menu-items 25)
-
+(use-package recentf
+  :ensure t
+  :config (progn
+	    (recentf-mode 1)
+	    (setq recentf-auto-cleanup 'never) ;; disable before we start recentf!
+	    (setq recentf-max-menu-items 25)))
 ;; Set file extension priority for ido-find-file
 (setq ido-file-extensions-order '(".org" ".tex" ".py" ".emacs" ".xml"))
-(require 'ido-vertical-mode)
-(ido-vertical-mode 1)
+(use-package ido-vertical-mode
+  :ensure t
+  :config (ido-vertical-mode 1))
 
 ;; icicles (fuzzy completion)
 (require 'apropos)
-(require 'fuzzy-match)
-(require 'hexrgb)
-(require 'icicles)
+(use-package fuzzy-match
+  :ensure t)
+(use-package hexrgb
+  :ensure t)
+(use-package icicles
+  :ensure t)
 
 ;; Find-line-in-buffer
 (load-file "~/.emacs.d/plugins/find-line-in-buffer.el")
@@ -33,31 +38,39 @@
 ;; Spell checker
 (autoload 'flyspell-mode "flyspell" "On-the-fly spelling checker." t)
 (setq flyspell-issue-welcome-flag nil) ;; fix flyspell problem
-(setq ispell-program-name "/usr/local/bin/aspell")
-(require 'ispell)
-(setq ispell-dictionary "english")
 (setq flyspell-sort-corrections nil)
+
+(use-package ispell
+  :ensure t
+  :init (setq ispell-program-name "/usr/local/bin/aspell")
+  :config (setq ispell-dictionary "english"))
 
 ;; Show matching parenthesis
 (show-paren-mode 1)
 
-(require 'magit)
-(setq magit-last-seen-setup-instructions "1.4.0")
+;; Magit
+(use-package magit
+  :ensure t
+  :config (setq magit-last-seen-setup-instructions "1.4.0"))
 
-(require 'yasnippet)
-(yas-global-mode 1)
-(setq yas-snippet-dirs (append yas-snippet-dirs '("~/.emacs.d/snippets")))
-(require 'dropdown-list)
-(setq yas-prompt-functions
-      '(yas-dropdown-prompt
-	yas-ido-prompt
-	yas-x-prompt
-	yas-completing-prompt
-	yas-no-prompt))
+;; Snippets
+(use-package dropdown-list)
+(use-package yasnippet
+  :ensure t
+  :config (progn
+	    (yas-global-mode 1)
+	    (setq yas-snippet-dirs (append yas-snippet-dirs '("~/.emacs.d/snippets")))
+	    (setq yas-prompt-functions
+		  '(yas-dropdown-prompt
+		    yas-ido-prompt
+		    yas-x-prompt
+		    yas-completing-prompt
+		    yas-no-prompt))))
 
 (setq password-cache-expiry nil)
 
-(require 'phi-search)
+(use-package phi-search
+  :ensure t)
 
 (load "~/.emacs.d/theme.el")
 (load "~/.emacs.d/languages.el")
@@ -66,7 +79,8 @@
 (load "~/.emacs.d/keybindings.el")
 (load "~/.emacs.d/helm-config.el")
 
-(require 'dirtree)
+(use-package dirtree
+  :ensure f)
 
 ;; Set default directory to home folder.
 (setq default-directory (concat (getenv "HOME") "/"))
@@ -85,8 +99,9 @@
   (mouse-wheel-mode -1))
 
 ;; Clipboard
-(require 'pbcopy)
-(turn-on-pbcopy)
+(use-package pbcopy
+  :ensure t
+  :config (turn-on-pbcopy))
 
 ;; Order buffers in ibuffer by major mode
 (setq ibuffer-default-sorting-mode 'major-mode)
@@ -94,15 +109,18 @@
 ;; Overwrite selection when yanking
 (delete-selection-mode 1)
 
-(require 'eww)
+;; Browser
+(use-package eww)
+
+;; Clean whitespace
 (add-hook 'before-save-hook 'whitespace-cleanup)
 
 ;; Indent
 ;;(global-aggressive-indent-mode)
 
 ;; Save place in files
-(require 'saveplace)
-(setq-default save-place t)
+(use-package saveplace
+  :config (setq-default save-place t))
 
 ;; Start emacs server
 (unless (server-running-p) (server-start))
@@ -133,6 +151,3 @@
 ;; Name perspective
 (load-file "~/.emacs.d/plugins/perspective-manager.el")
 (load-file "~/.emacs.d/perspectives.el")
-
-;; Magit default color
-(setq magit-log-arguments '("--graph" "--color" "--decorate"))
